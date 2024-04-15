cdfcd0da4ca099d1aae01dd48fad854d30db73b8
HFsa-RaShid <hafsa.cse7.bu@gmail.com>
2024-04-16 00:08:32 +0600

    banner responsive updated

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -24,32 +24,32 @@ const Banner = () => {
         onSlideChange={() => console.log('slide change')}
       >
         <SwiperSlide  style={{ height: "500px" }}>
-        <div className=" absolute top-[45%] w-[800px] rounded-xl left-[23%] bg-black opacity-50 px-8 py-5">
-                <h1 className="text-white text-2xl lg:text-[36px] mb-10 font-bold text-center">Get the best real estate deals first,
+        <div className=" absolute top-[15%] lg:top-[45%] w-[400px] lg:w-[800px] rounded-xl  md:left-[23%] bg-black opacity-50  px-4 py-2 lg:py-6">
+                <h1 className="text-white text-2xl lg:text-[36px] md:mb-2 lg:mb-10 font-bold text-center">Get the best real estate deals first,
                     </h1>
                     <h1 className="text-white text-2xl lg:text-[36px]  font-bold text-center">before they hit the mass market!</h1>
                     
                 </div>
             <img src={slider4} alt="Slide 1" /></SwiperSlide>
         <SwiperSlide  style={{ height: "500px" }}>
-        <div className=" absolute top-[45%] w-[800px] rounded-xl left-[23%] bg-black opacity-50 px-8 py-5 ">
-                <h1 className="text-white text-2xl lg:text-[36px] mb-10 font-bold text-center">Get the best real estate deals first,
+        <div className=" absolute top-[15%] lg:top-[45%] w-[400px] lg:w-[800px] rounded-xl  md:left-[23%] bg-black opacity-50  px-4 py-2 lg:py-6">
+                <h1 className="text-white text-2xl lg:text-[36px] md:mb-2 lg:mb-10 font-bold text-center">Get the best real estate deals first,
                     </h1>
                     <h1 className="text-white text-2xl lg:text-[36px]  font-bold text-center">before they hit the mass market!</h1>
                     
                 </div>
             <img src={slider2} alt="Slide 2" /></SwiperSlide>
         <SwiperSlide  style={{ height: "500px" }}>
-        <div className=" absolute top-[45%] w-[800px] rounded-xl left-[23%] bg-black opacity-50 px-8 py-5 ">
-                <h1 className="text-white text-2xl lg:text-[36px] mb-10 font-bold text-center">Get the best real estate deals first,
+        <div className=" absolute top-[15%] lg:top-[45%] w-[400px] lg:w-[800px] rounded-xl  md:left-[23%] bg-black opacity-50 px-4 py-2 lg:py-6">
+                <h1 className="text-white text-2xl lg:text-[36px] md:mb-2 lg:mb-10 font-bold text-center">Get the best real estate deals first,
                     </h1>
                     <h1 className="text-white text-2xl lg:text-[36px]  font-bold text-center">before they hit the mass market!</h1>
                     
                 </div>
             <img src={slider3} alt="Slide 3" /></SwiperSlide>
         <SwiperSlide  style={{ height: "500px" }}>
-        <div className=" absolute top-[45%] w-[800px] rounded-xl left-[23%] bg-black opacity-50 px-8 py-5">
-                <h1 className="text-white text-2xl lg:text-[36px] mb-10 font-bold text-center">Get the best real estate deals first,
+        <div className=" absolute top-[15%] lg:top-[45%] w-[400px] lg:w-[800px] rounded-xl  md:left-[23%] bg-black opacity-50  px-4 py-2 lg:py-6">
+                <h1 className="text-white text-2xl lg:text-[36px] md:mb-2 lg:mb-10 font-bold text-center">Get the best real estate deals first,
                     </h1>
                     <h1 className="text-white text-2xl lg:text-[36px]  font-bold text-center">before they hit the mass market!</h1>
                     
